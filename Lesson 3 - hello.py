1663f0ac353d3097b8744d48f438841c8537a5b1
Matthew Sage <mattsage@users.noreply.github.com>
2017-12-01 20:24:46 +0000

    Automate Boring things Course
    Automate Boring things Course

---
+++ Lesson 3 - hello.py
@@ -0,0 +1,15 @@
+# This program says hello and asks for my name
+
+print ('Hello World!')
+print ('What is your name?') #Ask for name
+
+myName = input()
+
+print ('Its nice to meet you, ' + myName)
+print('The length of your name is:')
+print (len(myName))
+print ('what is your age?') #Ask for age
+
+myAge= input()
+
+print ('You will be ' + str(int(myAge) +1) + ' in a year.')
